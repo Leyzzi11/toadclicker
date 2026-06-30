57e63eb0a34114e99a2c9f0bc9ecef68f3caeb38
Leyzzi11 <zeyroxxyacine2009@gmail.com>
2026-06-30 03:46:26 +0100

    Update Keys.h

--- src/Keys.h
+++ src/Keys.h
@@ -259,49 +259,104 @@ namespace toad
 
 	inline static const std::unordered_map<int, int> mc_as_vkc
 	{
-		{ 1, 27 },	// Escape
-		{ 2, 49 },	// 1
-		{ 3, 50 },	// 2
-		{ 4, 51 },	// 3
-		{ 5, 52 },	// 4
-		{ 6, 53 },	// 5
-		{ 7, 54 },	// 6
-		{ 8, 55 },	// 7
-		{ 9, 56 },	// 8
-		{ 10, 57 },	// 9
-		{ 11, 48 },	// 0
-		{ 12, 48 },	// -
-		{ 13, 48 },	// =
-		// { 14, VK_BACK }, // backspace
-		// { 15, VK_TAB  }, // tab
-		{ 16, 81 },	// Q
-		{ 17, 87 },	// W
-		{ 18, 69 },	// E
-		{ 19, 82 },	// R
-		{ 20, 84 },	// T
-		{ 21, 89 },	// Y
-		{ 22, 85 },	// U
-		{ 23, 73 },	// I
-		{ 24, 79 },	// O
-		{ 25, 80 },	// P
-		{ 30, 65 },	// A
-		{ 31, 83 },	// S
-		{ 32, 68 },	// D
-		{ 33, 70 },	// F
-		{ 34, 71 },	// G
-		{ 35, 72 },	// H
-		{ 36, 74 },	// J
-		{ 37, 75 },	// K 
-		{ 38, 76 },	// L
-		{ 44, 90 },	// Z
-		{ 45, 88 },	// X
-		{ 46, 67 },	// C
-		{ 47, 86 },	// V
-		{ 48, 66 },	// B
-		{ 49, 78 },	// N
-		{ 50, 77 },	// M
-		{ 58, 85 },	// caps lock
-
-		//mouse  ?
+		{ 1, VK_ESCAPE },	// Escape
+		{ 2, VK_1 },		// 1
+		{ 3, VK_2 },		// 2
+		{ 4, VK_3 },		// 3
+		{ 5, VK_4 },		// 4
+		{ 6, VK_5 },		// 5
+		{ 7, VK_6 },		// 6
+		{ 8, VK_7 },		// 7
+		{ 9, VK_8 },		// 8
+		{ 10, VK_9 },		// 9
+		{ 11, VK_0 },		// 0
+		{ 12, VK_OEM_MINUS },	// -
+		{ 13, VK_OEM_PLUS },	// =
+		{ 14, VK_BACK },	// Backspace
+		{ 15, VK_TAB },		// Tab
+		{ 16, 'Q' },		// Q
+		{ 17, 'W' },		// W
+		{ 18, 'E' },		// E
+		{ 19, 'R' },		// R
+		{ 20, 'T' },		// T
+		{ 21, 'Y' },		// Y
+		{ 22, 'U' },		// U
+		{ 23, 'I' },		// I
+		{ 24, 'O' },		// O
+		{ 25, 'P' },		// P
+		{ 26, VK_OEM_4 },	// [
+		{ 27, VK_OEM_6 },	// ]
+		{ 28, VK_RETURN },	// Enter
+		{ 29, VK_CONTROL },	// Left Ctrl
+		{ 30, 'A' },		// A
+		{ 31, 'S' },		// S
+		{ 32, 'D' },		// D
+		{ 33, 'F' },		// F
+		{ 34, 'G' },		// G
+		{ 35, 'H' },		// H
+		{ 36, 'J' },		// J
+		{ 37, 'K' },		// K
+		{ 38, 'L' },		// L
+		{ 39, VK_OEM_1 },	// ;
+		{ 40, VK_OEM_7 },	// '
+		{ 41, VK_OEM_3 },	// `
+		{ 42, VK_SHIFT },	// Left Shift
+		{ 43, VK_OEM_5 },	// \
+		{ 44, 'Z' },		// Z
+		{ 45, 'X' },		// X
+		{ 46, 'C' },		// C
+		{ 47, 'V' },		// V
+		{ 48, 'B' },		// B
+		{ 49, 'N' },		// N
+		{ 50, 'M' },		// M
+		{ 51, VK_OEM_COMMA },	// ,
+		{ 52, VK_OEM_PERIOD },	// .
+		{ 53, VK_OEM_2 },	// /
+		{ 54, VK_SHIFT },	// Right Shift
+		{ 55, VK_SNAPSHOT },	// Print Screen
+		{ 56, VK_MENU },	// Left Alt
+		{ 57, VK_SPACE },	// Space
+		{ 58, VK_CAPITAL },	// Caps Lock
+		{ 59, VK_F1 },		// F1
+		{ 60, VK_F2 },		// F2
+		{ 61, VK_F3 },		// F3
+		{ 62, VK_F4 },		// F4
+		{ 63, VK_F5 },		// F5
+		{ 64, VK_F6 },		// F6
+		{ 65, VK_F7 },		// F7
+		{ 66, VK_F8 },		// F8
+		{ 67, VK_F9 },		// F9
+		{ 68, VK_F10 },		// F10
+		{ 69, VK_F11 },		// F11
+		{ 70, VK_F12 },		// F12
+		{ 71, VK_SCROLL },	// Scroll Lock
+		{ 72, VK_PAUSE },	// Pause
+		{ 73, VK_INSERT },	// Insert
+		{ 74, VK_HOME },	// Home
+		{ 75, VK_PRIOR },	// Page Up
+		{ 76, VK_DELETE },	// Delete
+		{ 77, VK_END },		// End
+		{ 78, VK_NEXT },	// Page Down
+		{ 79, VK_RIGHT },	// Right Arrow
+		{ 80, VK_LEFT },	// Left Arrow
+		{ 81, VK_DOWN },	// Down Arrow
+		{ 82, VK_UP },		// Up Arrow
+		{ 83, VK_NUMLOCK },	// Num Lock
+		{ 84, VK_DIVIDE },	// Numpad /
+		{ 85, VK_MULTIPLY },	// Numpad *
+		{ 86, VK_SUBTRACT },	// Numpad -
+		{ 87, VK_ADD },		// Numpad +
+		{ 88, VK_RETURN },	// Numpad Enter
+		{ 89, VK_DECIMAL },	// Numpad .
+		{ 90, VK_NUMPAD0 },	// Numpad 0
+		{ 91, VK_NUMPAD1 },	// Numpad 1
+		{ 92, VK_NUMPAD2 },	// Numpad 2
+		{ 93, VK_NUMPAD3 },	// Numpad 3
+		{ 94, VK_NUMPAD4 },	// Numpad 4
+		{ 95, VK_NUMPAD5 },	// Numpad 5
+		{ 96, VK_NUMPAD6 },	// Numpad 6
+		{ 97, VK_NUMPAD7 },	// Numpad 7
+		{ 98, VK_NUMPAD8 },	// Numpad 8
+		{ 99, VK_NUMPAD9 },	// Numpad 9
 	};
 }
